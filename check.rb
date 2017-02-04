0bbf34afbf6b51a42f7e177b5c68ea3be68ce6c6
Alex Bepple <alex@bepple.de>
2017-02-04 01:04:32 +0100

    trigger build

--- check.rb
+++ check.rb
@@ -16,3 +16,6 @@ postgres_username = "postgres"
 postgres_password = ""
 test = PG.connect("postgres", 5432, "", "", "postgres", postgres_username, postgres_password)
 puts test.exec("SELECT version();").first["version"]
+
+puts "Doll!"
+
